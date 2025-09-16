9b08dff5f4e8c0db170289f601b2ade1927aeaf4
devansh joshi <devansh0814dj@gmail.com>
2025-09-16 14:41:38 +0530

    updated

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,127 @@
+import React from "react";
+import "../../styles/footer.css";
+import { BsLinkedin, BsGithub } from "react-icons/bs";
+import logo from "../../images/logo.png";
+import { useAuth } from "../../context/auth";
+
+const Footer = () => {
+  const [auth, setAuth] = useAuth();
+
+  return (
+    <>
+      {auth?.user?.role ? (
+        <>
+          <div>
+            <section id="contact" className="footer_wrapper">
+              <div className="container">
+                <div className="row">
+                  <div className="col-lg-5 footer_logo mb-4 mb-lg-0">
+                    <img decoding="async" src={logo} width={150} />
+                    <p className="footer_text" style={{ textAlign: "justify" }}>
+                      At Shield Her , we're dedicated to making  women saftey
+                      reality and we give our best at what we do
+                    </p>
+                  </div>
+                  <div className="col-lg-4 px-lg-5 mb-4 mb-lg-0">
+                    <h3 className="footer_title">Contact</h3>
+                    <p className="footer_text">
+                      <a>officialwomensafety@gmail.com</a>
+                      <br />
+                      <a className="footer-address">
+                        Madan Mohan Malaviya University of Technology University
+                        Gorakhpur<br />
+                        India
+                      </a>
+                    </p>
+                  </div>
+                  <div className="col-lg-3 mb-4 mb-lg-0">
+                    <h3 className="footer_title">Social Media</h3>
+                    <p>
+                      <a
+                        href="https://www.linkedin.com/in/devansh-joshi-12a036234/"
+                        className="footer_social_media_icon"
+                        style={{ color: "white" }}
+                      >
+                        <BsLinkedin size={25} />
+                      </a>
+                      <a
+                        href="https://github.com/thebotninja"
+                        className="footer_social_media_icon"
+                        style={{ color: "white" }}
+                      >
+                        <BsGithub size={25} />
+                      </a>
+                    </p>
+                  </div>
+                  <div className="col-12 footer_credits text-center">
+                    <span>
+                      © 2023 <a>Women Safety</a>™. All Rights Reserved.
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
+        </>
+      ) : (
+        <>
+          <div>
+            <section id="contact" className="footer_wrapper">
+              <div className="container">
+                <div className="row">
+                  <div className="col-lg-5 footer_logo mb-4 mb-lg-0">
+                    <img decoding="async" src={logo} width={150} />
+                    {/* <h1 className='text-sm' style={{fontSize:'25px'}}>WOMEN SECURE</h1> */}
+
+                    <p className="footer_text" style={{ textAlign: "justify" }}>
+                      At Shield Her , we're dedicated to making  women saftey
+                      reality and we give our best at what we do
+                    </p>
+                  </div>
+                  <div className="col-lg-4 px-lg-5 mb-4 mb-lg-0">
+                    <h3 className="footer_title">Contact</h3>
+                    <p className="footer_text">
+                      <a>officialwomensafety@gmail.com</a>
+                      <br />
+                      <a className="footer-address">
+                        Madan Mohan Malaviya University of Technology University
+                        Gorakhpur<br />
+                        India
+                      </a>
+                    </p>
+                  </div>
+                  <div className="col-lg-3 mb-4 mb-lg-0">
+                    <h3 className="footer_title">Social Media</h3>
+                    <p>
+                      <a
+                        href="https://www.linkedin.com/in/devansh-joshi-12a036234/"
+                        className="footer_social_media_icon"
+                        style={{ color: "white" }}
+                      >
+                        <BsLinkedin size={25} />
+                      </a>
+                      <a
+                        href="https://github.com/thebotninja"
+                        className="footer_social_media_icon"
+                        style={{ color: "white" }}
+                      >
+                        <BsGithub size={25} />
+                      </a>
+                    </p>
+                  </div>
+                  <div className="col-12 footer_credits text-center">
+                    <span>
+                      © 2023 <a>Women Safety</a>™. All Rights Reserved.
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
+        </>
+      )}
+    </>
+  );
+};
+
+export default Footer;
