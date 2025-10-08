b25c4a02e14f75ba452954546cee9232c4a459e3
devansh joshi <devansh0814dj@gmail.com>
2025-10-08 19:49:36 +0530

    update

--- src/pages/HeatMap.jsx
+++ src/pages/HeatMap.jsx
@@ -56,7 +56,7 @@ export default function Heatmap() {
       );
       if (res.status === 200) {
         // success
-        console.log(res.body)
+        console.log(res)
         setHeatPoints(res.body.points)
       }
     } catch (e) {
